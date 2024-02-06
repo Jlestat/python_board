4538054f8b55d7cb11ce4e0578f43e2edc34ad01
Jlestat <jlestatt@gmail.com>
2024-02-06 21:14:31 +0300

    create model Post

--- pythonboard/board/models.py
+++ pythonboard/board/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+
+class Post(models.Model):
+    title = models.CharField(verbose_name='Название объявления', max_length=150)
+    description = models.TextField(verbose_name='Описание')
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
